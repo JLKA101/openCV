9a56396bbb13d06b046a42e304949373f065b32d
JLKA101 <annewyp@yahoo.com>
2026-07-18 20:01:48 +0100

    Add files via upload

--- homework8bts.py
+++ homework8bts.py
@@ -31,27 +31,39 @@ mean_height = mean_height // 2
 
 print("Average Width: ", mean_width)
 print("Average Height: ", mean_height)
+resized_path = os.path.join(path, "resized_images")
 
+if not os.path.exists(resized_path):
+    os.mkdir(resized_path )
 for file in image_files:
     img = Image.open(os.path.join(path, file))
     width, height = img.size
 
     img.Resized = img.resize((mean_width, mean_height), Image.LANCZOS)
-    img.Resized.save(file, 'JPEG', quality=95)
+    img.Resized.save(os.path.join(resized_path, file), 'JPEG', quality=95)
     print(file, "is resized.")
 
-video_name = "bts_through_the_years.avi"
+video_name = "bts_through_the_years.mp4"
 
 frame = cv2.imread(image_files[0])
 height, width, layers = frame.shape #layers = colour channels
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID') #encoding video
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') #encoding video
 
 video = cv2.VideoWriter(video_name, fourcc, 1, (width, height)) #1 frame a second
 
-for image in image_files:
-    frame = cv2.imread(image)
-    video.write(frame)
+if not video.isOpened():
+    print("Video writer failed.")
+    exit()
+
+for i, image in enumerate(image_files):
+    frame = cv2.imread(os.path.join(path, image))
+    frame = cv2.resize(frame, (mean_width, mean_height))
+
+    if i ==0 :
+        cv2.putText(frame, "BTS", (170, 180), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 6, cv2.LINE_AA)
+
+        cv2.putText(frame, "Through The Years", (60, 260), cv2.FONT_HERSHEY_COMPLEX, 1.4, (220, 220, 220), 3, cv2.LINE_AA)
 
 video.release()
 
@@ -65,6 +77,7 @@ while True:
     ret, frame = cap.read()
 
     if not ret:
+        print("Unable to read frame.")
         break
 
     cv2.imshow("BTS Slideshow :3", frame)
